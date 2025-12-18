0160da42538277568c66afd2b682a70d37088a32
omkhanjodkar <okkhanjodkar@gmail.com>
2025-12-18 23:15:06 +0530

    Removing the old project that doesn't work

--- preload.js
+++
@@ -1,6 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('electronAPI', {
-  onFileOpened: (callback) => ipcRenderer.on('open-file-from-main', (event, filePath) => callback(filePath)),
-  readFileContent: (filePath) => ipcRenderer.invoke('read-file-content', filePath),
-});
